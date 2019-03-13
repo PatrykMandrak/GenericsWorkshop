50b4fb21159aea0d47eeede9112d1d55231edef0
WojTechM <wojciech.m1997@gmail.com>
2019-03-13 18:55:06 +0100

    Create base for generics demo

---
+++ src/main/java/com/github/wojtechm/Task1.java
@@ -0,0 +1,54 @@
+package com.github.wojtechm;
+
+/**
+ * Times before generics
+ *
+ * 1. Run java 4
+ * 2. Implement method workWithList()
+ * 3. See that its bad
+ * 4. Jump back to java 8+
+ * 5. Make sure it still compiles
+ * 6. Implement method workWithGenericList()
+ * 7. Compare implementations
+ *
+ * @author Wojciech Makiela
+ */
+public class Task1 {
+
+    public static void main(String[] args) {
+//        workWithList();
+    }
+
+    static void workWithList() {
+        // Create new list, and call it 'strings'
+
+        // Add strings 'first' and 'second' to your list
+
+
+        // Be a naughty boy/girl, and add an Integer '3'
+
+        // I know it looks bad, but keep going
+        // Get first element from your list, and assign it
+        // to a variable of type String
+
+        // Do the same for last element
+
+        // Run the method
+    }
+
+    static void workWithGenericList() {
+        // Create new list of type <String>, and call it 'strings'
+
+        // Add strings 'first' and 'second' to your list
+
+
+        // Be a naughty boy/girl, and add an Integer '3'
+
+        // Hah! Compile error! Comment your 'addInt' line
+        // Get first element from your list, and assign it
+        // to a variable of type String
+
+        // No casts!
+        // Run the method
+    }
+}
