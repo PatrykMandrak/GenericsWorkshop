acb93408fbac9d97f31dada766ce0659b07fa447
WojTechM <wojciech.m1997@gmail.com>
2019-03-14 22:04:22 +0100

    Finish Task6

--- src/main/java/com/github/wojtechm/Task6.java
+++ src/main/java/com/github/wojtechm/Task6.java
@@ -1,7 +1,9 @@
 package com.github.wojtechm;
 
 
+import java.util.Arrays;
 import java.util.List;
+import java.util.function.Consumer;
 
 /**
  * Bounded Parameters! Part 2!
@@ -39,23 +41,70 @@ public class Task6 {
     static class ElvisPresley extends Singer implements Cloneable<ElvisPresley> {
         private boolean isBestSinger = true;
 
-        ElvisPresley(String genre) {
-            super(genre);
+        ElvisPresley() {
+            super("Rock n' Roll");
         }
 
         @Override
         public ElvisPresley clone() {
-            return new ElvisPresley(super.genre);
+            return new ElvisPresley();
         }
     }
 
     public static void main(String[] args) {
 
+        /*
+        We already had method that accepts any type of Singer
+        Now, lets update the way it behaves.
+         */
+
+        Consumer<Singer> convertToKPopSinger = singer -> singer.genre = "KPop";
+        List<Singer> singers = Arrays.asList(new ElvisPresley());
+        workWithSingers(singers, convertToKPopSinger);
+        System.out.println(singers.get(0).genre); // Elvis is now KPop king! Oh no!
+
+        /*
+        Quick info about Consumer
+        It accepts parameter of given type (Singer in this case), and processes it in one way or another
+        You do not need to know anything else at this moment
+         */
+
+        // But here's the problem
+        Consumer<Object> print = System.out::println;
+        singers = Arrays.asList(new ElvisPresley()); // Back to old, Rock n roll Elvis
+//        workWithSingers(singers, print); // Compile error! Uncomment to check it out!
+
+        /*
+            I know that Singer can be printed.
+            You know that too.
+            But the compiler does not.
+            It expects Consumer of Singers, not a Consumer of Objects.
+            What to do now? <T extends Singer>? Maybe <?>?.
+            Damn it. It's not working! What to do, what to do...
+            If we use 'extends', it's expecting a subclass of a Singer. But we don't need a subclass.
+            We need a super class!
+
+
+            For now, you were using 'extends' only - defining an "Upper Bound" of type.
+            Or in other words, You were defining the 'highest' class in inheritance/family tree
+
+                Object
+                   |
+                Person
+                   |
+                Singer
+                   |
+                Presley
+
+            But now we need something above Singer, not below. In order to define "Lower Bound"
+            You must use 'super' keyword!
+            I'll let you figure out how to do that
+         */
     }
 
-    private static void workWithSingers(List<? extends Singer> singers) {
+    private static void workWithSingers(List<? extends Singer> singers, Consumer<Singer> consumer) {
         for (Singer singer : singers) {
-            System.out.println(singer);
+            consumer.accept(singer);
         }
     }
 }
