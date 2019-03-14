5b20e3ef6dc8c60f0ac03a23e1238b487dbbe8ee
WojTechM <wojciech.m1997@gmail.com>
2019-03-14 20:11:53 +0100

    Finished Task4

--- src/main/java/com/github/wojtechm/Task4.java
+++ src/main/java/com/github/wojtechm/Task4.java
@@ -35,7 +35,34 @@ public class Task4 {
 
     private static Cloneable cloneSinger(Cloneable cloneable) {
         return (Cloneable) cloneable.clone();
-        // Look at that ugly cast! Why is it here?
-        // Delete '(Cloneable)' and read error description.
+        /*
+        Look at that ugly cast! Why is it here?
+        Delete '(Cloneable)' and read error description.
+
+        'clone' method returns Object, not Cloneable. And it makes sense.
+        If you take a look at our Cloneable interface declaration, you can see that 'T' might be anything.
+        And since (almost) everything in Java is an Object, T is considered one as well.
+        You can fix that by using 'extends' keyword!
+
+        If you define Cloneable as 'Cloneable<T extends Cloneable>, You will be sure that clone method
+        won't return anything that is not Cloneable.
+        It makes sense to think, that if You clone something, then clone itself should be cloneable, doesn't it?
+
+        Once You update Cloneable, this ugly cast won't be necessary... but we are not done here yet!
+        Below you can see an example of parametrized method. You do that by defining new type (T) before return type.
+        Now. Once you know how to define type in method, and how to use extends keyword with generics
+        fix 'cloneSinger' method.
+        'cloneSinger' should accept only parameters that are subtype of 'Singer',
+        and that implement 'Cloneable' interface (with generics, we use 'extends' keyword
+        for both classes and interfaces).
+
+        
+        HINT: Your generic type can extend more that one class. To do that, use '&' ;)
+
+        */
+    }
+
+    private static <T> T returnT(T t) {
+        return t;
     }
 }
